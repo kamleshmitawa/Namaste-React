35cca866b9e91977a89dd46a276adede7f058bf9
Kamlesh <kmitawa@q3tech.com>
2024-01-13 19:44:45 +0530

    episode2: notes are created for 2nd episode and added npm scripts with parcel

---
+++ dist/index.613c6637.js
@@ -0,0 +1,2 @@
+var e=ReactDOM.createRoot(document.getElementById("root"));console.log(React.createElement("h1",{id:"heading",xyz:"abc"},"Hello React !!!!"));var t=React.createElement("div",{id:"parent"},[React.createElement("div",{id:"child1"},[React.createElement("h1",{},"this is heading 1"),React.createElement("h2",{},"this is heading 2")]),React.createElement("div",{id:"child2"},[React.createElement("h1",{},"this is heading 1"),React.createElement("h2",{},"this is heading 2")])]);e.render(t);
+//# sourceMappingURL=index.613c6637.js.map
